9643c3d5930b923feb65beb0d32854a343c808ea
HBalyan <82547236+HBalyan@users.noreply.github.com>
2021-05-04 20:57:11 +0400

    add 2 files

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
     });
-
+   
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
